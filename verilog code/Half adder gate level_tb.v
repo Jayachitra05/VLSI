679725853c53e1c1e2cdd7dbc95e5bda9b263b8a
Jayachitra05 <163533937+Jayachitra05@users.noreply.github.com>
2024-09-25 16:36:29 +0530

    Create Half adder gate level_tb.v

---
+++ verilog code/Half adder gate level_tb.v
@@ -0,0 +1,19 @@
+ module tb_half adder;
+  reg a, b;
+  wire s, c;
+  half_adder ha(a, b, s, c);
+  initial begin
+    $monitor("At time %0t: a=%b b=%b, sum=%b, carry=%b",$time, a,b,s,c);
+    a = 0; b = 0;
+    #1;
+    a = 0; b = 1;
+    #1;
+    a = 1; b = 0;
+    #1;
+    a = 1; b = 1;
+  end
+   initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+   end
+endmodule 
