d711c8aa1fc4d32f0f8f59e1fba253732a79284e
Jayachitra05 <163533937+Jayachitra05@users.noreply.github.com>
2024-10-01 22:16:26 +0530

    Create 2:1 mux data flow tb.v

---
+++ verilog code/2:1 mux data flow tb.v
@@ -0,0 +1,26 @@
+module tb_mux_2to1_dataflow;
+  reg A;
+  reg B;
+  reg S;
+  wire Y;
+  mux_2to1_dataflow uut (
+    .A(A), 
+    .B(B), 
+    .S(S), 
+    .Y(Y)
+  );
+  initial begin
+    A=0;
+    B=0;
+    S=0;
+  
+    $monitor("At time %t: S = %b, A = %b, B = %b -> Y = %b", $time, S, A, B, Y);
+    #10 A = 0; B = 1; S = 0;  
+    #10 A = 1; B = 0; S = 1;  
+    #10 A = 1; B = 1; S = 0;  
+    #10 A = 0; B = 1; S = 1;  
+    #10 A = 1; B = 1; S = 1;  
+    #10 A = 0; B = 0; S = 0;  
+    #50 $finish;
+  end
+endmodule
