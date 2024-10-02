0d980590c47a651eea701fceb781d3bc5d0ee3c0
Jayachitra05 <163533937+Jayachitra05@users.noreply.github.com>
2024-10-02 17:59:27 +0530

    Create string using operators.v

---
+++ verilog code/string using operators.v
@@ -0,0 +1,17 @@
+module tb;
+  string firstname = "VLSI";
+  string lastname  = "Design";  
+  initial begin
+    if (firstname == lastname)
+      $display ("firstname=%s is EQUAL to lastname=%s", firstname, lastname);
+    if (firstname != lastname)
+      $display ("firstname=%s is NOT EQUAL to lastname=%s", firstname, lastname);
+    if (firstname < lastname)
+      $display ("firstname=%s is LESS THAN lastname=%s", firstname, lastname);  
+    if (firstname > lastname)
+      $display ("firstname=%s is GREATER THAN lastname=%s", firstname, lastname); 
+    $display ("Full Name = %s", {firstname, " ", lastname});  
+    $display ("%s", {3{firstname}}); 
+    $display ("firstname[2]=%s lastname[2]=%s", firstname[2], lastname[2]); 
+  end
+endmodule
