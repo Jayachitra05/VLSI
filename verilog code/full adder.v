e08ad04cc9fb6d8150710e73a833a8b6098cc300
Jayachitra05 <163533937+Jayachitra05@users.noreply.github.com>
2024-10-03 09:08:58 +0530

    Update full adder.v

--- verilog code/full adder.v
+++ verilog code/full adder.v
@@ -1,4 +1,4 @@
-module full_adder(input a, b, cin, output S, Cout);
-  assign S = a ^ b ^ cin;
+module full_adder(input a, b, cin, output s, c);
+  assign s = a ^ b ^ cin;
   assign Cout = (a & b) | (b & cin) | (a & cin);
 endmodule
