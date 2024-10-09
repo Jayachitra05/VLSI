5b45507b8836385c53477b19f5e156dfc9c090a2
Jayachitra05 <163533937+Jayachitra05@users.noreply.github.com>
2024-10-09 14:26:15 +0530

    Create while loop increment.v

---
+++ verilog code/Assignment questions/while loop increment.v
@@ -0,0 +1,11 @@
+module while_example;
+    reg [3:0] acc;
+
+    initial begin
+        acc = 4'b0000; 
+        while (acc < 4'b1000) begin 
+            acc = acc + 1;  
+        end
+        $display("Final value of acc = %b", acc);
+    end
+endmodule
