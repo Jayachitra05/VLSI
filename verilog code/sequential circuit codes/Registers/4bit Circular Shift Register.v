ed54a615ae3c5a8641dcacea7a45204d6e30e025
Jayachitra05 <163533937+Jayachitra05@users.noreply.github.com>
2024-11-03 05:29:09 +0530

    Create 4bit Circular Shift Register.v

---
+++ verilog code/sequential circuit codes/Registers/4bit Circular Shift Register.v
@@ -0,0 +1,13 @@
+module circular_shift_register (
+    input clk,
+    input rst,
+    input d,
+    output reg [3:0] q
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst) 
+            q <= 0;
+        else 
+            q <= {q[2:0], q[3]}; // Circular shift left
+    end
+endmodule
