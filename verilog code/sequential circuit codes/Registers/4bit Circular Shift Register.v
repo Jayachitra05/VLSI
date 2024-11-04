52310d192f251f495e7082bf5b3f35a00b89e1f3
Jayachitra05 <163533937+Jayachitra05@users.noreply.github.com>
2024-11-04 11:41:11 +0530

    Update 4bit Circular Shift Register.v

--- verilog code/sequential circuit codes/Registers/4bit Circular Shift Register.v
+++ verilog code/sequential circuit codes/Registers/4bit Circular Shift Register.v
@@ -1,17 +1,1 @@
-module circular_shift_register (
-    input clk,
-    input rst,
-    input d,
-    output reg [3:0] q
-);
-    always @(posedge clk or posedge rst) begin
-        if (rst) 
-            q <= 0;
-        else 
-            q <= {q[2:0], q[3]}; // Circular shift left
-    end
-    initial begin
-    $dumpfile("waves.vcd");
-    $dumpvars;
-    end
-endmodule
+
