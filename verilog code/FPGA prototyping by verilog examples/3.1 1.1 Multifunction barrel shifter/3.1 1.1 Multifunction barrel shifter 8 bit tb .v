78a9c7c50f849fb33f179945f8ab1914041a9efe
Jayachitra05 <163533937+Jayachitra05@users.noreply.github.com>
2024-11-11 21:20:22 +0530

    Create 3.1 1.1 Multifunction barrel shifter 8 bit tb .v

---
+++ verilog code/FPGA prototyping by verilog examples/3.1 1.1 Multifunction barrel shifter/3.1 1.1 Multifunction barrel shifter 8 bit tb .v
@@ -0,0 +1,19 @@
+module tb_barrel;
+  reg [7:0] a;
+  reg [2:0] amt;
+  reg lr;
+  wire [7:0] y;
+  multifn_barrel_shifter barrel(a,amt,lr,y);
+  initial begin
+    a = 8'b1010_1010; amt = 3'b001; lr = 0; #10; 
+    a = 8'b1010_1010; amt = 3'b010; lr = 0; #10; 
+    a = 8'b1010_1010; amt = 3'b001; lr = 1; #10; 
+    a = 8'b1010_1010; amt = 3'b010; lr = 1; #10; 
+    $monitor("At time %t : a=%b, amt=%b, lr=%b, y=%b",$time,a,amt,lr,y);
+    $finish;
+  end
+  initial begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+  end
+endmodule
